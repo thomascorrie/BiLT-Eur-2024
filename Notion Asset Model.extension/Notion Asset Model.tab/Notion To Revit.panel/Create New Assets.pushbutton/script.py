8f391a1ebb041cafd9b631cfeeec6a5af3cce93e
thomascorrie <thomas_corrie@hotmail.com>
2024-05-18 23:12:44 +0100

    Corrected handling of no new assets to add in Create New Assets

--- Notion Asset Model.extension/Notion Asset Model.tab/Notion To Revit.panel/Create New Assets.pushbutton/script.py
+++ Notion Asset Model.extension/Notion Asset Model.tab/Notion To Revit.panel/Create New Assets.pushbutton/script.py
@@ -84,56 +84,57 @@ if fam_sym:
         ).get("results")
 
     # Process results
-    if len(results) > 0:
-        for result in results:
-            mark_value = result['properties']['ID']['unique_id']['prefix'] + "-" + str(result['properties']['ID']['unique_id']['number'])
-            if mark_value in marks:
-                continue
-            report = mark_value
-            space_name = None
+    processed = 0
+    for result in results:
+        mark_value = result['properties']['ID']['unique_id']['prefix'] + "-" + str(result['properties']['ID']['unique_id']['number'])
+        if mark_value in marks: # Check whether asset already exists in the model
+            continue
+        report = mark_value
+        space_name = None
+        space_element = None
+        # Space
+        try:
+            space_id = result['properties']['Spaces']['relation'][0]['id']
+            space_name = dict_space_id_name[space_id]
+            space_element = dict_spaces_id_room[space_id]
+        except:
             space_element = None
-            # Space
-            try:
-                space_id = result['properties']['Spaces']['relation'][0]['id']
-                space_name = dict_space_id_name[space_id]
-                space_element = dict_spaces_id_room[space_id]
-            except:
-                space_element = None
-            # Size
-            try:
-                size = result['properties']['Size (w*d*h)']['rich_text'][0]['plain_text']
-                width = int(size.split('x')[0])
-                depth = int(size.split('x')[1])
-                height = int(size.split('x')[2])
-                width_f = width / 304.8 # Revit API in decimal feet
-                depth_f = depth / 304.8 # Revit API in decimal feet
-                height_f = height / 304.8 # Revit API in decimal feet
-            except:
-                size = None
-            # Create Element in Revit
-            with Transaction('SetParam') as rvtxn:
-                report = report + " " + result['properties']['Name']['title'][0]['plain_text']
-                if space_element:
-                    e = doc.Create.NewFamilyInstance(space_element.Location.Point, fam_sym, DB.Structure.StructuralType.NonStructural)
-                    report = report + " placed in " + space_name
-                else:
-                    e = doc.Create.NewFamilyInstance(DB.XYZ[0,0,0], fam_sym, DB.Structure.StructuralType.NonStructural)
-                mark = e.GetParameters("Mark")[0]
-                mark.Set(mark_value)
-                comments = e.GetParameters("Comments")[0]
-                comments.Set(result['properties']['Name']['title'][0]['plain_text'])
-                if size:
-                    width_param = e.GetParameters("Width")[0]
-                    width_param.Set(width_f)
-                    depth_param = e.GetParameters("Depth")[0]
-                    depth_param.Set(depth_f)
-                    height_param = e.GetParameters("Height")[0]
-                    height_param.Set(height_f)
-                    report = report + " " + str(width) + "x" + str(depth) + "x" + str(height)
-                else:
-                    report = report + " no size given"
-                print(report)
-    else:
+        # Size
+        try:
+            size = result['properties']['Size (w*d*h)']['rich_text'][0]['plain_text']
+            width = int(size.split('x')[0])
+            depth = int(size.split('x')[1])
+            height = int(size.split('x')[2])
+            width_f = width / 304.8 # Revit API in decimal feet
+            depth_f = depth / 304.8 # Revit API in decimal feet
+            height_f = height / 304.8 # Revit API in decimal feet
+        except:
+            size = None
+        # Create Element in Revit
+        with Transaction('SetParam') as rvtxn:
+            report = report + " " + result['properties']['Name']['title'][0]['plain_text']
+            if space_element:
+                e = doc.Create.NewFamilyInstance(space_element.Location.Point, fam_sym, DB.Structure.StructuralType.NonStructural)
+                report = report + " placed in " + space_name
+            else:
+                e = doc.Create.NewFamilyInstance(DB.XYZ[0,0,0], fam_sym, DB.Structure.StructuralType.NonStructural)
+            mark = e.GetParameters("Mark")[0]
+            mark.Set(mark_value)
+            comments = e.GetParameters("Comments")[0]
+            comments.Set(result['properties']['Name']['title'][0]['plain_text'])
+            if size:
+                width_param = e.GetParameters("Width")[0]
+                width_param.Set(width_f)
+                depth_param = e.GetParameters("Depth")[0]
+                depth_param.Set(depth_f)
+                height_param = e.GetParameters("Height")[0]
+                height_param.Set(height_f)
+                report = report + " " + str(width) + "x" + str(depth) + "x" + str(height)
+            else:
+                report = report + " no size given"
+            print(report)
+        processed = processed + 1
+    if processed == 0:
         print("No new assets to place in the model")
 else:
     print("Generic Asset Family Type Not Found")
